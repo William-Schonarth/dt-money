00242f2e72885d79faf5c3d79319941bf4f12bcb
william.schonarth <williams@microsys.inf.br>
2023-11-13 17:04:17 -0300

    feat: finished UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoSvg from '../../assets/logo.svg'
+import { NewTransactionsModal } from "../NewTransactionsModal"
 
 function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoSvg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionsModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
